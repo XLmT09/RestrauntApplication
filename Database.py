fc733a91b7e704ef5ea5ec1ff2cd14fb21dcc58c
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-01-17 19:10:00 +0000

    Created a table for storing each dish on the menu.
    I have used SQL to create a table within the database for storing information about the
    different dishes on the menu including price, calories and ingredients.

--- Database.py
+++ Database.py
@@ -18,14 +18,20 @@ class Database:
         self.executeQuery(connection, tableDescription)
         print("Table " + tableName + " successfully created")
 
+
 if __name__ == "__main__":
     try:
         myDatabase = Database()
         dbConnection = myDatabase.connect()
         print("successfully connected to database!")
 
+        #Create a table for storing login information about staff and customers
         loginTableSql = "CREATE TABLE loginInfo(username varchar(10), password varchar(10) NOT NULL, role varchar(15), primary key(username))"
         myDatabase.createTable(dbConnection, "loginInfo", loginTableSql)
+
+        #Create a table for storing information about each dish on the menu
+        dishTableSql = "CREATE TABLE Dish(dishID int, name varchar(20), price int, calories int, ingredients varchar(50), primary key(dishID))"
+        myDatabase.createTable(dbConnection, "Dish", dishTableSql)
 
     except Exception:
         print("Error connecting to database")
