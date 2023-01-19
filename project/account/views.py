66b32ffc21f37f0c5550d76c4e6c3926bad2ea97
Hijithan <zjac082@live.rhul.ac.uk>
2023-01-19 23:32:57 +0000

    Added flash messages on sign up page
    Give error message if sugn up failed or a success message otherwise.

--- project/account/views.py
+++ project/account/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
+from django.contrib import messages
+
 # Create your views here.
 def login(request):
     return render(request, 'account/login.html')
@@ -12,7 +14,10 @@ def signup(request):
             # Store values in the field directly to the database
             form.save()
             username = form.cleaned_data.get('username')
-            return redirect('account-login')
+            messages.success(request, f'Account created for {username}!')
+        # If the user clicks submit but data wasnt valid
+        else:
+            messages.error(request, f'Account failed to be created!')
     # Load a form for the user
     else:
         form = UserRegisterForm()
