e6d2148cf14e306a9aff77cec135f4700ae9ab40
Hijithan <zjac082@live.rhul.ac.uk>
2023-01-20 14:31:22 +0000

    Implemented backend functionality of login
    User can now login to there account after creation, error messages will
    be displayed if wrong login info is given.

--- project/account/url.py
+++ project/account/url.py
@@ -1,7 +1,11 @@
 from django.urls import path
+#import pre built login aand logout views by django
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name = 'account-login'),
+    # template name shows where the html file is in the templates folder.
+    # this is only done for urls where views were pre built for java
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name = 'account-login'),
     path('signup/', views.signup, name = 'account-signup')
 ]
