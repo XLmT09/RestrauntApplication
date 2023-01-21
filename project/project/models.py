97060560245cfe78b675452557628b6ae3e1234a
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-01-21 18:29:00 +0000

    Created the table for menuItems using django.
    Using the table definition in Database.py, I have created the table for menu items within django so
    that the code feels more connected. The table contains multiple attributes including the name, price and ingredients of each item.

--- project/project/models.py
+++ project/project/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItem(models.Model):
+    ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    calories = models.IntegerField()
+    ingredients = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=10, null=True)
+
+    class Meta:
+        db_table = "MenuItem"
