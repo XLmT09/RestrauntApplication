ca13915953cfa93ae534b2f8bc6bef19a30182af
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-01-21 19:25:17 +0000

    Using django, I have added a table for customer orders.
    This table is based on the existing definition in the Database.py file and stores information about a customer's order
    such as the status of the order and which items from the menu have been ordered by the customer.

--- project/project/models.py
+++ project/project/models.py
@@ -1,14 +1,35 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
 class MenuItem(models.Model):
+    class Meta:
+        db_table = "MenuItem"
+
     ID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     price = models.FloatField()
     calories = models.IntegerField()
-    ingredients = models.CharField(max_length=100)
     cuisine = models.CharField(max_length=10, null=True)
+    ingredients = ArrayField(models.TextField())
+    
 
+class Order(models.Model):
     class Meta:
-        db_table = "MenuItem"
+        db_table = "Order"
+
+    class orderStatus(models.TextChoices):
+        PLACED = "Placed"
+        CONFIRMED = "Confirmed"
+        DELIVERED = "Delivered"
+
+    ID = models.AutoField(primary_key=True)
+    customerID = models.IntegerField() #link to User table as foreign key
+    status = models.CharField(max_length=10,choices=orderStatus.choices) #add enums for status
+    timeOfOrder = models.TimeField()
+    orderedItems = ArrayField(models.IntegerField(), null=True)
+
+    
+
+    
