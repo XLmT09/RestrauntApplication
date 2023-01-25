f8a182abc9b8559c7ba16ae8a1fb27ad6746c0eb
ZKAC423 <ZKAC423@live.rhul.ac.uk>
2023-01-25 17:58:36 +0000

    Inserted the models to begin testing for code coverage.

--- Oaxaca/restaurant/models.py
+++ Oaxaca/restaurant/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
+from account import forms
 
 # Create your models here.
+
+class MenuItem(models.Model):
+    class Meta:
+        db_table = "MenuItem"
+
+    class MenuItemCourse(models.TextChoices):
+        STARTER = "Starter"
+        MAIN = "Main"
+        DESSERT = "Dessert"
+        SIDE = "Side"
+        DRINK = "Drink"
+
+    class MenuItemRequirements(models.TextChoices):
+        VEGAN = "Vegan"
+        VEGETARIAN = "Vegetarian"
+
+    ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    calories = models.IntegerField()
+    cuisine = models.CharField(max_length=10, null=True)
+    ingredients = ArrayField(models.TextField())
+
+    course = models.CharField(max_length=10,choices=MenuItemCourse.choices, null=False, default="Main")
+    dietRequirements = models.BooleanField(max_length=10,choices=MenuItemRequirements.choices, null=True)
+    
+
+class Order(models.Model):
+    class Meta:
+        db_table = "Order"
+
+    class orderStatus(models.TextChoices):
+        PLACED = "Placed"
+        CONFIRMED = "Confirmed"
+        DELIVERED = "Delivered"
+
+    ID = models.AutoField(primary_key=True)
+    customerID = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10,choices=orderStatus.choices)
+    timeOfOrder = models.TimeField()
+    orderedItems = ArrayField(models.IntegerField(), null=True)
