f431ca3e433801f583bb57ba73b45c46420960d3
ZKAC200 <ZKAC200@live.ac.uk>
2023-01-25 14:25:40 +0000

    Adds idividual menu items with styles for them

--- project/project/views.py
+++ project/project/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from. import nutritionPopUp
 
-foodList = [nutritionPopUp.item("food","55"),nutritionPopUp.item("food3","65"),nutritionPopUp.item("food2",75)]
-
+foodList = [nutritionPopUp.item("food","55"),nutritionPopUp.item("food3","65"),nutritionPopUp.item("food2",75),
+            nutritionPopUp.item("food","55"),nutritionPopUp.item("food3","65"),nutritionPopUp.item("food2",75),
+            nutritionPopUp.item("food","55"),nutritionPopUp.item("food3","65"),nutritionPopUp.item("food2",75),
+            nutritionPopUp.item("food","55"),nutritionPopUp.item("food3","65"),nutritionPopUp.item("food2",75),
+            nutritionPopUp.item("food","55"),nutritionPopUp.item("food3","65"),nutritionPopUp.item("food2",75)]
 def homePage(request):
     # this selects the name of the web page and sends the user to that page
     return render(request, 'homePage.html', {'title': 'Home'})
