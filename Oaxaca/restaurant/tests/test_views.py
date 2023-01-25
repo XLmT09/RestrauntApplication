3553a1af517d1fb75f4a371b2afdfd25afebd806
ZKAC423 <ZKAC423@live.rhul.ac.uk>
2023-01-25 18:51:27 +0000

    Added the third test for views of the restaurant app. bitdotio does not currently support the creation of a test database.

---
+++ Oaxaca/restaurant/tests/test_views.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.urls import reverse
+
+#Test views (Uses Reverse)
+
+class HomePageTest(TestCase):
+    def test_restaurant_view1(self):
+        menuItem = self.create_MenuItem()
+        url = reverse("restaurant.views.homePage")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(menuItem.title, response.content)
+
+    def test_restaurant_view2(self):
+        order = self.create_Order()
+        url = reverse("restaurant.views.homePage")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(order.title, response.content)
+
+class MenuTest(TestCase):
+    def test_restaurant_menu1(self):
+        menuItem = self.create_MenuItem()
+        url = reverse("restaurant.views.menu")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(menuItem.title, response.content)
