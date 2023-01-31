37aee0c531d1b9d907a4437fc1b0027c409155da
Mjakota Ivans (2021) ZKAC261 <ivans.mjakota.2021@live.rhul.ac.uk>
2023-01-31 22:54:37 +0000

    Added forms for price, calories and cuisine
    Added forms to represent the simpler variables of the food items in the
    MenuUpdateForm

--- project/waiter/forms.py
+++ project/waiter/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+
+
 class menuUpdateForm(forms.Form):
     name = forms.CharField(label="name", max_length=20)
+    price = forms.FloatField(label = "price")
+    calories = forms.IntegerField(label="calories")
+    cuisine = forms.CharField(label="cuisine", max_length=20)
+    #ingredients
+    #course
+    #dietRequirements
