8353f5326c1a07cccf17427aafff295a3f2f8815
Mjakota Ivans (2021) ZKAC261 <ivans.mjakota.2021@live.rhul.ac.uk>
2023-02-01 00:21:17 +0000

    Added the remaining forms for menuUpdateForm
    Added the drop down menus for course and dietRequirements following the same logic as the ingredients form

--- project/waiter/forms.py
+++ project/waiter/forms.py
@@ -6,11 +6,23 @@ INGREDIENTS = [
     ("Vegetables", "Vegetables")
 ]
 
+COURSE = [
+    ("Starter", "Starter")
+    ("Main", "Main"),
+    ("Dessert", "Dessert")
+]
+
+DIET = [
+    ("None", "None"),
+    ("Vegeterian","Vegeterian"),
+    ("Pesciterian","Pesciterian")
+]
+
 class menuUpdateForm(forms.Form):
     name = forms.CharField(label="name", max_length=20)
     price = forms.FloatField(label = "price")
     calories = forms.IntegerField(label="calories")
     cuisine = forms.CharField(label="cuisine", max_length=20)
     ingredients = forms.CharField(label = 'Select ingredients', widget = forms.Select(choices = INGREDIENTS))
-    #course
-    #dietRequirements
+    course = forms.CharField(label = 'Select course', widget = forms.Select(choices = COURSE))
+    dietRequirements = forms.CharField(label = 'Select dietary requirements', widget = forms.Select(choices = DIET))
