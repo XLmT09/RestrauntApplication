93f2b464aa7ddf7d7985a196233ff4f3e9caf85b
Mjakota Ivans (2021) ZKAC261 <ivans.mjakota.2021@live.rhul.ac.uk>
2023-02-01 00:15:13 +0000

    Added a drop down menu for ingredients
    Used forms in order to add a simple drop down menu for ingredients

--- project/waiter/forms.py
+++ project/waiter/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 
-
+INGREDIENTS = [
+    ("Meat", "Meat"),
+    ("Cheese", "Cheese"),
+    ("Vegetables", "Vegetables")
+]
 
 class menuUpdateForm(forms.Form):
     name = forms.CharField(label="name", max_length=20)
     price = forms.FloatField(label = "price")
     calories = forms.IntegerField(label="calories")
     cuisine = forms.CharField(label="cuisine", max_length=20)
-    #ingredients
+    ingredients = forms.CharField(label = 'Select ingredients', widget = forms.Select(choices = INGREDIENTS))
     #course
     #dietRequirements
