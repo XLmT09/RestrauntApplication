c7a5b5dc50f3603ded36473f1a430006482e89be
ZKAC200 <ZKAC200@live.ac.uk>
2023-02-02 17:05:22 +0000

    Adds a checkout button that shows total price of order

--- project/project/static/javascript/menu.js
+++ project/project/static/javascript/menu.js
@@ -1,31 +1,23 @@
-function popup(name, price) {
-  console.log("yes");
-  document.getElementById("menuBox").style.zIndex = -1;
-  document.getElementById("menuBox").style.opacity = 0.2;
-  document.getElementById("orderBox").style.zIndex = 1;
-  document.getElementById("orderBox").style.opacity = 1;
-  document.getElementById("name").innerHTML = name;
-  document.getElementById("price").innerHTML = price;
-}
-
-function exitpopup() {
-  document.getElementById("menuBox").style.zIndex = 1;
-  document.getElementById("menuBox").style.opacity = 1;
-  document.getElementById("orderBox").style.zIndex = -1;
-  document.getElementById("orderBox").style.opacity = 0;
-}
-
-function addItem(id, addSubtract) {
+function addItem(id, addSubtract, price) {
   var element = document.getElementById(id);
   var text = element.textContent;
   var numberOfItems = parseInt(text);
-  console.log(numberOfItems);
+
+  var basket = document.getElementById("basket");
+  var priceText = parseFloat(basket.textContent);
+  console.log(typeof priceText);
+  var newPrice = parseFloat(price);
+  console.log(typeof newPrice);
+
+  var numberOfItems = parseInt(text);
+
   if (numberOfItems > 0 && addSubtract < 0) {
     numberOfItems = numberOfItems - 1;
     element.innerHTML = numberOfItems;
+    basket.innerHTML = parseFloat(priceText) - parseFloat(newPrice);
   } else if (addSubtract > 0) {
-    console.log("yes");
     numberOfItems = numberOfItems + 1;
     element.innerHTML = numberOfItems;
+    basket.innerHTML = parseFloat(priceText) + parseFloat(newPrice);
   }
 }
