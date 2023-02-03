c7613781459c5ecef534db8f21994b9b3d0c9608
Hijithan <zjac082@live.rhul.ac.uk>
2023-02-03 15:36:59 +0000

    Test signup page loads up
    wrote a unittest to check that signup page loads up.

--- project/account/tests/test_views.py
+++ project/account/tests/test_views.py
@@ -2,6 +2,12 @@ from django.test import TestCase
 
 # Create your tests here.
 class TestView(TestCase):
-    def test_loginpage(self):
+    # Test if login page loads up
+    def test_login_page(self):
         response = self.client.get("/account/login/")
+        self.assertEqual(response.status_code, 200)
+
+    # Test if signup page loads up
+    def test_signup_page(self):
+        response = self.client.get("/account/signup/")
         self.assertEqual(response.status_code, 200)
