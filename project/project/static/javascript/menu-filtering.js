632ec09c9da000d44b45a9a8082222730f560460
ZKAC423 <ZKAC423@live.rhul.ac.uk>
2023-02-05 10:14:35 +0000

    Fix containers not re-appearing after filtering

--- project/project/static/javascript/menu-filtering.js
+++ project/project/static/javascript/menu-filtering.js
@@ -2,6 +2,10 @@ function hide(selectedOption) {
     optionValue = selectedOption.value;
 
     for (var container of document.getElementsByClassName("course")) {
+        container.style.display = "block";
+    }
+
+    for (var container of document.getElementsByClassName("course")) {
         if (container.id != optionValue) {
             container.style.display = "none";
         }
