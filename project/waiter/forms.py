e29deca438c612240adb15ba6fd2cb30ad0fce55
Mjakota Ivans (2021) ZKAC261 <ivans.mjakota.2021@live.rhul.ac.uk>
2023-02-05 17:26:56 +0000

    Added a Meta class to menuUpdateForm
    This class contains the code that will be used in the "view" for the waiter in order to delete an existing item from the database

--- project/waiter/forms.py
+++ project/waiter/forms.py
@@ -9,3 +9,10 @@ class menuUpdateForm(forms.ModelForm):
     class Meta:
         model = MenuItem
         fields = ["name", "price", "calories", "cuisine", "ingredients", "course", "dietRequirements"]
+
+    class Meta:
+        model = MenuItem
+        def delete(self, comit=True):
+            self.instance.delete()
+            if comit:
+                self.instance.save()
