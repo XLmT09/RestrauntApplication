f86c0d4b5de5435bac8bb74304608d337a13f1da
Hijithan <zjac082@live.rhul.ac.uk>
2023-02-07 14:44:29 +0000

    Test if correct HTML page is used for login page

--- project/account/tests/test_views.py
+++ project/account/tests/test_views.py
@@ -5,9 +5,12 @@ class TestView(TestCase):
     # Test if login page loads up
     def test_login_page(self):
         response = self.client.get("/account/login/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200, 
+        "Check if login page loads up.")
+        self.assertTemplateUsed(response, "account/login.html", 
+        "Check if correct html template is being used")
 
     # Test if signup page loads up
     def test_signup_page(self):
         response = self.client.get("/account/signup/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200, "Check if signup page loads up.")
