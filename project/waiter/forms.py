ce1b7292de1967ac716018655cfd9296e0f84475
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-02-07 11:57:48 +0000

    Added a button to clear the menu item form
    I have added a button at the bottom of the form (for updating menu items) that when pressed, will clear any text within the text boxes and will display a blank form to the user.

--- project/waiter/forms.py
+++ project/waiter/forms.py
@@ -18,16 +18,6 @@ DIETREQUIREMENTS = [
 
 
 class menuUpdateForm(forms.ModelForm):
-    dropDown = forms.ModelMultipleChoiceField(queryset=MenuItem.objects.all())
-
-    name = forms.CharField(widget=forms.TextInput(attrs={'id':'nameField'}))
-    price = forms.FloatField(widget=forms.NumberInput(attrs={'id':'priceField'}))
-    calories = forms.IntegerField(widget=forms.NumberInput(attrs={'id':'caloriesField'}))
-    cuisine = forms.CharField(widget=forms.TextInput(attrs={'id':'cuisineField'}))
-    ingredients = forms.CharField(widget=forms.TextInput(attrs={'id':'ingredientsField'}))
-    course = forms.CharField(widget=forms.Select(choices=COURSES,attrs={'id':'courseField'}))
-    dietRequirements = forms.CharField(widget=forms.Select(choices=DIETREQUIREMENTS,attrs={'id':'dietRequirementsField'}))
-
     class Meta:
         model = MenuItem
         fields = ["name", "price", "calories", "cuisine", "ingredients", "course", "dietRequirements"]
