08385d6b5eb4f1457440c29c4d44719106e51693
ZKAC423 <ZKAC423@live.rhul.ac.uk>
2023-02-08 07:03:37 +0000

    Finish search bar implementation for the menu

--- project/project/static/javascript/menu-searching.js
+++ project/project/static/javascript/menu-searching.js
@@ -1,3 +1,30 @@
 function search(searchText) {
-    searchValue = searchText.value;
+    var searchValue = searchText.value;
+    searchValue = searchValue.toLowerCase();
+
+    var itemContainers = document.getElementsByClassName("container");
+    var mainBox = document.getElementsByClassName("dishbox2")[0];
+
+    mainBox.style.visibility = "visible";
+
+    for (var showContainer of itemContainers) {
+        showContainer.style.visibility = "visible";
+    }
+
+    for (var itemContainer of itemContainers) {
+        itemHeader = itemContainer.getElementsByClassName("header")[0];
+        itemName = itemHeader.getElementsByClassName("name")[0];
+        itemNameValue = itemName.innerHTML.toLowerCase();
+        
+        if (itemNameValue == searchValue) {
+            for (var hideContainer of itemContainers) {
+                hideContainer.style.visibility = "hidden";
+            }
+            
+            mainBox.style.visibility = "hidden";
+            itemContainer.style.visibility = "visible";
+            
+            previousItemContainer = itemContainer;
+        }
+    }
 }
