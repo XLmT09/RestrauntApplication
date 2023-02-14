a3394106216ea2b3561e4a4734a16790bc19dca8
Hijithan <zjac082@live.rhul.ac.uk>
2023-02-14 21:54:32 +0000

    Only kitchen users can acess kitchen page
    If the user is not a kitchen staff they get redirected to the login page.

--- project/kitchen/views.py
+++ project/kitchen/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from project.models import Order
+from django.contrib.auth.decorators import user_passes_test
 
+#This lambda expression checks if the user is logged in and has the correct permissions
+# If the user does not meet requirments they get redirected to login page
+@user_passes_test(lambda u: u.groups.filter(name="KitchenStaff").exists())
 # This page wil contain a list of all the confirmed customer orders for the kitchen staff
 def order(request):
     # Make a querey to get all customer orders which are confirmed sorted from oldest to newest
