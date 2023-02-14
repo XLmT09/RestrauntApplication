42a01564b3f83ea935d260e972d88287805ee631
Hijithan <zjac082@live.rhul.ac.uk>
2023-02-14 20:23:47 +0000

    Display a message when no customer orders
    Display a message on html page when there is no customer orders instead
    of a blank html page.

--- project/kitchen/views.py
+++ project/kitchen/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from project.models import Order
 
+# This page wil contain a list of all the confirmed customer orders for the kitchen staff
 def order(request):
+    # Make a querey to get all customer orders which are confirmed sorted from oldest to newest
     cust_orders = Order.objects.filter(status="Confirmed").order_by('timeOfOrder')
+    # list of confimred orders are passed to the html page
     return render(request, 'order.html', {'title': 'Kitchen Staff Orders','cust_orders': cust_orders})
