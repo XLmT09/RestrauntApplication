bf8d6f62890588643e33b57cc37d48466300645b
Hijithan <zjac082@live.rhul.ac.uk>
2023-02-14 15:26:46 +0000

    Add base html template for kitchen staff app
    I add a base html file which contains the html navbar code to avoid
    duplicating the code in each html file.

--- project/kitchen/views.py
+++ project/kitchen/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def order(request):
-    return render(request, 'kitchen/order.html', {'title': 'Kitchen Staff Order'})
+    return render(request, 'order.html', {'title': 'Kitchen Staff Order'})
