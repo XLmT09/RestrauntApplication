3f478ba8ba3f572a917bc93d7e087e58f0515918
Hijithan <zjac082@live.rhul.ac.uk>
2023-02-14 15:47:12 +0000

    Make querey for all confirmed customers
    When user loads up kitchen staff page, all orders which are confirmed
    should be gathered.

--- project/kitchen/views.py
+++ project/kitchen/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from project.models import Order
 
 def order(request):
-    return render(request, 'order.html', {'title': 'Kitchen Staff Orders'})
+    cust_orders = Order.objects.filter(status="Confirmed").order_by('timeOfOrder')
+    return render(request, 'order.html', {'title': 'Kitchen Staff Orders','cust_orders': cust_orders})
