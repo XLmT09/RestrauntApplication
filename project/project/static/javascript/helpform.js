81f0ce825cabd427c6c7291f02a02fb6445ac468
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-02-17 20:54:23 +0000

    Added a pop-up form for submitting help requests
    I have added a pop up window that appears when the "Request Help" button is pressed and allows the customer to add a message when requesting for help.

---
+++ project/project/static/javascript/helpform.js
@@ -0,0 +1,8 @@
+function openHelpForm(){
+    document.getElementById("myHelpForm").style.display = "block";
+}
+
+
+function submitHelpForm(){
+    document.getElementById("myHelpForm").style.display = "none";
+}
