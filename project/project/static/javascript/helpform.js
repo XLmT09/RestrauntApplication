45f9480371973ae36022ddb646353e955e58c380
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-02-17 22:34:33 +0000

    Added a "close" button to the help form
    This button allows the customer to close the help form without having to submit an empty request.

--- project/project/static/javascript/helpform.js
+++ project/project/static/javascript/helpform.js
@@ -4,14 +4,6 @@ function openHelpForm(){
 
 
 function closeHelpForm(){
-    m = document.getElementById("messageEntry").innerText;
-    alert("you have entered:", m)
     document.getElementById("myHelpForm").style.display = "none";
 }
 
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + exdays * 3 * 1000);
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
