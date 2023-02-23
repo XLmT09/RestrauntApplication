c175f00472c0b7da1346c9b86409eca0dc2a2b0d
ZKAC200 <ZKAC200@live.ac.uk>
2023-02-23 16:36:06 +0000

    Fixes isses from pulling from main

--- project/waiter/urls.py
+++ project/waiter/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('updateMenu', views.changeMenu, name = 'changeMenu'),
     path('updateMenu/<str:item>', views.refreshMenu, name = 'refreshMenu'),
-    path('', views.staff, name = 'staffPage'),
+    path('staff/', views.staff, name = 'staffPage'),
     path("<str:orderStatus> orders", views.viewOrders, name = "viewOrders"),
     path("deleteItem/<str:itemToDelete>", views.deleteItem, name="deleteItem"),
 
