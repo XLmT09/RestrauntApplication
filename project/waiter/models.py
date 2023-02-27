28520eb1e315728f1b43df8acaee47558a259840
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-02-27 15:18:15 +0000

    Adds a django model for tracking payments
    Adds a django model called "Payment" which stores information about payments within the restaurant's website such as payment amount and the ID of the customer who made a given payment.

--- project/waiter/models.py
+++ project/waiter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Payment(models.Model):
+    class Meta:
+        db_table = "Payment"
+
+    paymentID = models.AutoField(primary_key=True)
+    customerID = models.ForeignKey(User, on_delete=models.CASCADE,null=False)
+    timeOfPayment = models.TimeField(auto_now=True, null=False)
+    paymentAmount = models.FloatField(null=False)
