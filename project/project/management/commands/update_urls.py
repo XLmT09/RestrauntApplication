07fa9032de6bc259719dfc5e3c00192ed7caf83d
ZKAC423 <ZKAC423@live.rhul.ac.uk>
2023-03-01 07:33:49 +0000

    Perform URL addition for first starter

--- project/project/management/commands/update_urls.py
+++ project/project/management/commands/update_urls.py
@@ -5,3 +5,4 @@ from project.models import MenuItem
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        MenuItem.objects.filter(name = 'Chicken Taquitos').update(imgurl = 'https://drive.proton.me/urls/WQEAJ7N6D8#r4xpYnfCgFOg')
