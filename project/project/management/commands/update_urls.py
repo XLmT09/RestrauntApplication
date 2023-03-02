82746ee8da7a39fcd4af3a0543c54f0dfb06fff8
ZKAC423 <ZKAC423@live.rhul.ac.uk>
2023-03-02 14:23:13 +0000

    Complete URL additions for starters

--- project/project/management/commands/update_urls.py
+++ project/project/management/commands/update_urls.py
@@ -5,4 +5,9 @@ from project.models import MenuItem
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        # URL's for menu starters
         MenuItem.objects.filter(name = 'Chicken Taquitos').update(imgurl = 'https://drive.proton.me/urls/WQEAJ7N6D8#r4xpYnfCgFOg')
+        MenuItem.obejcts.filter(name = 'Nachos').update(imgurl = 'https://drive.proton.me/urls/2EPT1Y71J8#oddAauKkfajp')
+        MenuItem.obejcts.filter(name = 'Elote').update(imgurl = 'https://drive.proton.me/urls/G9C83QPSPW#4XR1jCZgCHVu')
+        MenuItem.obejcts.filter(name = 'Cheviche').update(imgurl = 'https://drive.proton.me/urls/TA5GXZRCC0#E47ClL6sE6a8')
+        MenuItem.obejcts.filter(name = 'Black Bean Salsa').update(imgurl = 'https://drive.proton.me/urls/2FHQ3TA15M#xcaOcnQdHOht')
