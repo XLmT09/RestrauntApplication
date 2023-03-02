b121b93860894426eddef0db6735c62b3aca9211
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-03-02 11:48:50 +0000

    Updates price attribute of MenuItem model
    Updates the price attribute to a decimal field so that the prices of each item are correctly displayed on the menu to two decimal places.

--- project/project/models.py
+++ project/project/models.py
@@ -23,7 +23,7 @@ class MenuItem(models.Model):
         VEGETARIAN = "Vegetarian"
 
     name = models.CharField(max_length=30, primary_key=True)
-    price = models.FloatField(null=True)
+    price = models.DecimalField(max_digits=6,decimal_places=2,null=False)
     calories = models.IntegerField(null=True)
    
     alergies = models.TextField()
