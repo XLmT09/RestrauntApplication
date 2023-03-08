77f18475fb87113e3fb7b0cf1875835da576c201
Mjakota Ivans (2021) ZKAC261 <ivans.mjakota.2021@live.rhul.ac.uk>
2023-03-08 23:54:02 +0000

    Added ability to delete help request in database
    I have added the code that now allows the waiter to delete the help request from the database

--- project/project/views.py
+++ project/project/views.py
@@ -113,3 +113,5 @@ def sendHelpRequest(request):
 def clientHelpRequests(request):
     help_requests = HelpRequest.objects.all()
     return render(request, 'clientHelpRequests.html', {'help_requests': help_requests})
+
+
