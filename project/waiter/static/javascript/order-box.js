fed383d534dee8a1eb075fffb6034906db9f670a
Hijithan <zjac082@live.rhul.ac.uk>
2023-03-09 17:11:31 +0000

    Add a rotate button to the box
    I add a rotate button to the box so that it is more intutive to the user
    that the ordered items are on the back of the box.

---
+++ project/waiter/static/javascript/order-box.js
@@ -0,0 +1,6 @@
+// Grab the relevent box the user wants to rotate by id
+function rotate(boxid) {
+    const orderBoxOuter = document.getElementById(boxid);
+    // Apply the relvent css to actiavte the roation
+    orderBoxOuter.focus()
+}
