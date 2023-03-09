f26e9e08e0c16752a39e4cfb6bfea0aa55122616
Hijithan <zjac082@live.rhul.ac.uk>
2023-03-09 19:57:44 +0000

    Add a roate btn at back of box
    When the waiter flips the card over, they should be able to also flip
    the xard back to its front, so a button has been added to do this.

--- project/waiter/static/javascript/order-box.js
+++ project/waiter/static/javascript/order-box.js
@@ -1,6 +1,12 @@
 // Grab the relevent box the user wants to rotate by id
 function rotate(boxid) {
     const orderBoxOuter = document.getElementById(boxid);
-    // Apply the relvent css to actiavte the roation
-    orderBoxOuter.focus()
+    console.log("hi");
+    if (orderBoxOuter.classList.contains('rotate')) {
+        // remove the class to deactivate the rotation
+        orderBoxOuter.classList.remove('rotate');
+      } else {
+        // add the class to activate the rotation
+        orderBoxOuter.classList.add('rotate');
+      }
 }
