d4368991cdfafb518725a6780c5be636e7ec1577
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-03-13 19:43:27 +0000

    Adds a popup alert when logging out
    Displays a popup message alerting the user they have been logged out, before returning to the home page.

--- project/account/url.py
+++ project/account/url.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # this is only done for urls where views were pre built for java
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name = 'account-login'),
     # we use a template for logout to because by default it will redirect to the admin page
-    path('logout/', auth_views.LogoutView.as_view(), {'next-page':settings.LOGOUT_REDIRECT_URL}, name = 'logoutUser'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next-page':settings.LOGOUT_REDIRECT_URL}, name = 'account-logout'),
     path('profile/', views.profile, name='account-profile'),
     path('signup/', views.signup, name = 'account-signup'),
 ]
