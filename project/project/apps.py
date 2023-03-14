ffa69a1e0ad0fc2ee04a25e9c1ac4879b3fefb60
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-03-14 15:39:02 +0000

    Adds a view to refresh the database upon loading the server
    When the server is run, the Order table is cleaned-up and any old orders exceeding the current date of the system are deleted from the database.

--- project/project/apps.py
+++ project/project/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+class MainAppConfig(AppConfig):
+    name = 'project'
+    def ready(self):
+        print("START UP CODE")
+
 
 class ManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
--- project/project/urls.py
+++ project/project/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+from .views import cleanupDatabase
+cleanupDatabase()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homePage , name = 'home'),
