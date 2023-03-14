cc93b7a84b86dc171bcc768647f21a6972328c24
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-03-14 14:06:39 +0000

    Adds new date field to Order table
    Adds a new attribute to the order table for storing the date an order is placed on.

--- project/project/models.py
+++ project/project/models.py
@@ -51,6 +51,7 @@ class Order(models.Model):
     status = models.CharField(max_length=10, choices=orderStatuses.choices, null=False, default="Placed")
     # Will automatically get the current time due to the auto_now attrbuite
     timeOfOrder = models.TimeField(auto_now=True, null=False)
+    dateOfOrder = models.DateField(auto_now=True, null=False)
     orderedItems = models.JSONField(null=False)
     notificationSent = models.BooleanField(default=False)
 
