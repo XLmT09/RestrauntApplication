742b639712d7c482ec87468ba428cc0b96c64107
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-03-16 12:28:56 +0000

    Adds a collapisble menu to the order page for waiters
    When the order page is loaded, a collapisble menu is now shown allowing the user to select between different types of orders.

--- project/waiter/static/javascript/orderPopup.js
+++ project/waiter/static/javascript/orderPopup.js
@@ -6,3 +6,22 @@ function displayPopup(){
 function hidePopup(){
     document.getElementById("orderPopup").style.display = "none";
 }
+
+function showOrders(){
+    alert("shpwing orders");
+}
+
+var menuChoices = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < menuChoices.length; i++) {
+    menuChoices[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
