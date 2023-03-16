e2667a1bfc4b6af2f9e150362e4a306dcc768624
zkac226 <ZKAC226@live.rhul.ac.uk>
2023-03-16 15:30:14 +0000

    Adjusts how orders can be viewed in the dropdown menu
    Only shows one type of orders at a time by hiding any that have been previously selected.

--- project/waiter/static/javascript/orderPopup.js
+++ project/waiter/static/javascript/orderPopup.js
@@ -3,12 +3,18 @@ var i;
 
 for (i = 0; i < menuChoices.length; i++) {
     menuChoices[i].addEventListener("click", function() {
-    this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
       content.style.display = "block";
+    }
+    var j;
+    for (j = 0; j < menuChoices.length; j++){
+        if (menuChoices[j] != this) {
+            var content = menuChoices[j].nextElementSibling;
+            content.style.display = "none";
+        }
     }
   });
 }
